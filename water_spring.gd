962dfa943be66698ec27f5e3560f563feb9b40f7
Oskar Kunik <oskar.kunik@gmail.com>
2023-07-06 20:17:54 +0200

    Add dampening

--- water_spring.gd
+++ water_spring.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 # The spring's current velocity
 var velocity: int = 0
 
@@ -14,12 +13,13 @@ var height: float = position.y
 var target_height: float = position.y + 200
 
 # The spring stiffness constant
-var k: float = 0.015
+var k: float = 0.04
+
+# The spring dampening value
+var d = 0.04
 
 
-func water_update(spring_constant):
-	print(target_height)
-
+func water_update(spring_constant, dampening):
 	# This function applies the Hooke's Law force to the spring
 	# This function will be called in each frame
 
@@ -29,8 +29,10 @@ func water_update(spring_constant):
 	# How far the spring is currently extended
 	var x = height - target_height
 
+	var loss = -dampening * velocity
+
 	# Hooke's Law implementation for spring behavior
-	force = - spring_constant * x
+	force = - spring_constant * x + loss
 
 	# Apply the force to velocity
 	velocity += force
@@ -38,8 +40,5 @@ func water_update(spring_constant):
 	# Move the pring
 	position.y += velocity
 
-	pass
-
-
 func _physics_process(_delta: float) -> void:
-	water_update(k)
+	water_update(k, d)
